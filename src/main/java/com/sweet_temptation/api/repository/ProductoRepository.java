7ee062f49e575d8a9067549869b4de2ce587d529
Max Jimenez <maximiliano.soto.j@gmail.com>
2025-11-12 23:17:48 -0600

    fix: controller pedido
    Se creó el endpoint para calcular el total de pedido

--- src/main/java/com/sweet_temptation/api/repository/ProductoRepository.java
+++ src/main/java/com/sweet_temptation/api/repository/ProductoRepository.java
@@ -4,13 +4,14 @@ import com.sweet_temptation.api.dto.DetallesProductoDTO;
 import com.sweet_temptation.api.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface ProductoRepository extends JpaRepository<Producto,Integer> {
 
-    @Query("SELECT pp.id AS id, pp.cantidad AS cantidad, p.nombre AS nombre, p.precio AS precio, pp.subtotal AS subtotal, p.id AS idProducto\n" +
-            " FROM Producto p JOIN ProductoPedido pp ON p.id = pp.idProducto \n" +
-            " WHERE pp.idPedido = :idPedido")
-    List<DetallesProductoDTO> obtenerListaProductos(int idPedido);
+    @Query("SELECT new com.sweet_temptation.api.dto.DetallesProductoDTO(pp.id, pp.cantidad, p.nombre, p.precio, pp.subtotal, p.id) FROM Producto p JOIN ProductoPedido pp ON p.id = pp.idProducto WHERE pp.idPedido = :idPedido")
+
+    List<DetallesProductoDTO> obtenerListaProductos(@Param("idPedido") int idPedido);
 
 }
