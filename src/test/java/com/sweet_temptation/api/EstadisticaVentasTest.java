b5a7744e769eacb7f1885682fff636f179eb00ac
Ness-luna <moralesvanessa2010@gmail.com>
2025-11-18 10:07:57 -0600

    Agregar pruebas estadisticas ventas

--- src/test/java/com/sweet_temptation/api/EstadisticaVentasTest.java
+++ src/test/java/com/sweet_temptation/api/EstadisticaVentasTest.java
@@ -4,13 +4,13 @@ import com.sweet_temptation.api.dto.PedidoDTO;
 import com.sweet_temptation.api.model.Pedido;
 import com.sweet_temptation.api.repository.PedidoRepository;
 import com.sweet_temptation.api.servicios.PedidoService;
+import com.sweet_temptation.api.validaciones.PedidoValidator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.management.Query;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,7 +19,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -27,8 +27,11 @@ public class EstadisticaVentasTest {
     @Mock
     private PedidoRepository pedidoRepository;
 
+    @Mock
+    private PedidoValidator validaciones;
+
     @InjectMocks
-    private PedidoService reporteVentasService;
+    private PedidoService pedidoService;
 
     @Test
     void consultarVentas_Exito(){
@@ -40,15 +43,34 @@ public class EstadisticaVentasTest {
         pedido.setTotal(BigDecimal.valueOf(200));
         pedido.setEstado(3);
         pedido.setFechaCompra(LocalDateTime.now());
+        pedido.setActual(false);
+        pedido.setPersonalizado(false);
+        pedido.setIdCliente(10);
+
+        doNothing().when(validaciones)
+                .validarRangoFecha(any(LocalDateTime.class), any(LocalDateTime.class));
+
+        when(validaciones.validarEstadoVenta("completada")).thenReturn(3);
 
         when(pedidoRepository.findByEstadoAndFechaCompra(
-                any(), any(), eq(3)
+                any(LocalDateTime.class),
+                any(LocalDateTime.class),
+                eq(3)
         )).thenReturn(List.of(pedido));
 
-        List<PedidoDTO> resultado = reporteVentasService.consultarVentasPorRangoYEstado(inicio, fin, "completada");
+        List<PedidoDTO> resultado = pedidoService.consultarVentasPorRangoYEstado(
+                inicio, fin, "completada"
+        );
 
         assertEquals(1, resultado.size());
         assertEquals(1, resultado.get(0).getId());
+        assertEquals(BigDecimal.valueOf(150), resultado.get(0).getTotal());
+
+        verify(validaciones, times(1)).validarRangoFecha(any(LocalDateTime.class), any(LocalDateTime.class));
+
+        verify(validaciones, times(1)).validarEstadoVenta("completada");
+
+        verify(pedidoRepository, times(1)).findByEstadoAndFechaCompra(any(LocalDateTime.class), any(LocalDateTime.class), eq(3));
     }
 
 }
