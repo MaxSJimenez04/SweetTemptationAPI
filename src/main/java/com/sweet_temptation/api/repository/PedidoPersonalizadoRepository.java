079de901448d24bf1b89af242ca20f495f866ce2
Ness-luna <moralesvanessa2010@gmail.com>
2025-12-06 00:23:20 -0600

    Eliminar producto

--- src/main/java/com/sweet_temptation/api/repository/PedidoPersonalizadoRepository.java
+++ src/main/java/com/sweet_temptation/api/repository/PedidoPersonalizadoRepository.java
@@ -2,6 +2,9 @@ package com.sweet_temptation.api.repository;
 
 import com.sweet_temptation.api.model.PedidoPersonalizado;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PedidoPersonalizadoRepository extends JpaRepository<PedidoPersonalizado,Integer> {
 }
